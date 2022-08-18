31978d91a3a9df901eb92b1821db2b2ee7fe7e31
speechrezz <thespeechlessmusic@gmail.com>
2022-08-17 18:23:49 -0700

    Improved header look on mobile

--- src/lib/comp/Navbar.svelte
+++ src/lib/comp/Navbar.svelte
@@ -1,14 +1,14 @@
 <script>
-  const baseLook = "link-nav px-6 h-full flex align-middle border-b-2 border-t-2 border-t-base-1 ";
+  const baseLook = "flex-1 lg:flex-none link-nav px-6 h-full flex justify-center align-middle border-b-2 border-t-2 border-t-base-1 ";
+  //const baseLook =                   "link-nav px-6 h-full flex                align-middle border-b-2 border-t-2 border-t-base-1 ";
   const look = baseLook + "border-b-slate-600 hover:border-b-slate-500";
   const lookHighlighted = baseLook + "border-b-accent-1 hover:border-b-accent-1-bright";
 
   export let index = 0;
 </script>
 
-
 <nav class="bg-base-1 w-full h-16 flex justify-center leading-[normal] fixed z-50">
-  <div class="w-main flex lg:justify-between align-middle justify-center">
+  <div class="w-full lg:w-main flex lg:justify-between align-middle justify-center">
 
     <!-- Left links -->
     <a href="/" class="link-nav h-full lg:flex align-middle hidden">
