31397ea4828b0b4c04e0c750491ac109671d8d6a
speechrezz <thespeechlessmusic@gmail.com>
2022-08-22 18:38:05 -0700

    Now only a single video can play at a time

--- src/lib/comp/Rezonator/MutableVideo.svelte
+++ src/lib/comp/Rezonator/MutableVideo.svelte
@@ -1,4 +1,6 @@
 <script>
+  import { onMount } from 'svelte';
+  import MutableVideoStore from "$lib/stores/MutableVideoStore"
   export let videoPath = "vid/MidiDemo.mp4";
 
   let vidElement;
@@ -7,7 +9,26 @@
   const toggleMute = () => {
     vidElement.muted = !vidElement.muted;
     muteElement.src = vidElement.muted ? "svg/volume-x.svg" : "svg/volume.svg";
+    if (vidElement.muted === false) {
+      $MutableVideoStore = videoPath;
+    }
   }
+
+  let muteVideoCallback = () => {};
+
+  $: $MutableVideoStore, muteVideoCallback();
+
+  // Only want muteVideoCallback() to work when everything is loaded
+  onMount(() => {
+    muteVideoCallback = () => {
+      if ($MutableVideoStore !== videoPath) {
+        vidElement.muted = true;
+        muteElement.src = "svg/volume-x.svg";
+      }
+    }
+  })
+  
+
 </script>
 
 <div class="lg:hover:scale-[102%] lg:transition-all w-fit">
---
+++ src/lib/stores/MutableVideoStore.js
@@ -0,0 +1,5 @@
+import { writable } from "svelte/store";
+
+const MutableVideoStore = writable("");
+
+export default MutableVideoStore;
