b2e062ac00104cb604ea0c21bb96bf98644792dd
speechrezz <thespeechlessmusic@gmail.com>
2022-08-24 10:30:59 -0700

    Simplified footer

--- src/lib/comp/Footer.svelte
+++ src/lib/comp/Footer.svelte
@@ -1,9 +1,9 @@
 <nav class="bg-base-1 w-full h-16 flex justify-center leading-[normal]">
   <div class="flex justify-center align-middle">
     <span class="text-xs text-neutral-1 m-auto md:text-base">
-      Copyright © 2022 Xynth Audio. All rights reserved. 
-      <a href="/legal/terms" class="link-footer">Terms & conditions</a> 
-      <span class="text-slate-500">|</span>
+      Copyright © 2022 Xynth Audio. All rights reserved.
+      <!-- <a href="/legal/terms" class="link-footer">Terms & conditions</a> 
+      <span class="text-slate-500">|</span> -->
       <a href="/legal/eula" class="link-footer">License agreement</a> 
       <span class="text-slate-500">|</span>
       <a href="/about" class="link-footer">Contact</a>
