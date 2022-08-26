f9a2609b74b95c23fd3191dc7029264062c806dc
speechrezz <thespeechlessmusic@gmail.com>
2022-08-25 17:33:23 -0700

    Improved mobile view

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -22,19 +22,19 @@
     </div>
     <div class="absolute hidden xl:block w-full overflow-clip -z-20">
       <div class="relative bg-base-4 rounded-full   
-        w-[28rem] h-[28rem] -top-2 -left-2 transition-all ease-out duration-300"
+        w-[28rem] h-[28rem] -top-2 -left-2 transition-all ease-out"
         style:transform="translateY({scroll * 0.1}px)" />
       <div class="relative bg-accent-2 rounded-full 
-        w-[15rem] h-[15rem]  top-36 left-80 transition-all ease-out duration-300"
+        w-[15rem] h-[15rem]  top-36 left-80 transition-all ease-out duration-500"
         style:transform="translateY({scroll * -0.15}px)" />
 
       <div class="relative bg-accent-1 rounded-full 
         w-[10rem] h-[10rem] bottom-[26rem] left-[calc(100vw-29rem)] z-10
-        transition-all ease-out duration-300"
+        transition-all ease-out duration-500"
         style:transform="translateY({scroll * -0.18}px)" />
       <div class="relative bg-base-4 rounded-full   
         w-[46rem] h-[46rem] bottom-[40rem] left-[calc(100vw-29rem)]
-        transition-all ease-out duration-300" 
+        transition-all ease-out" 
         style:transform="translateY({scroll * 0.2}px)"/>
     </div>
   </div>
@@ -43,7 +43,7 @@
 
   <!-- Section 2 -->
   <div class="bg-neutral-1 flex flex-col px-2 pt-12 lg:flex-row justify-center lg:px-32 lg:space-x-24 lg:pt-8">
-    <div class="w-main mb-8 animate-fadeIn mx-auto">
+    <div class="sm:w-main mb-8 animate-fadeIn mx-auto">
       <Article />
     </div>
   </div>
