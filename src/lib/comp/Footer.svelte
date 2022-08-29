1e3a52185899cba94cc7cbec92700fd5d8b8bf9f
speechrezz <thespeechlessmusic@gmail.com>
2022-08-29 13:55:36 -0700

    Fixed issue with footer

--- src/lib/comp/Footer.svelte
+++ src/lib/comp/Footer.svelte
@@ -1,6 +1,6 @@
 <nav class="bg-base-1 w-full h-16 flex justify-center leading-[normal]">
   <div class="flex justify-center align-middle">
-    <div class="text-xs text-neutral-1 m-auto md:text-base flex flex-col sm:flex-row">
+    <div class="text-xs text-neutral-1 m-auto md:text-base flex flex-col sm:flex-row sm:space-x-1">
       <span>Copyright © 2022 Xynth Audio. All rights reserved.</span>
       <span class="mx-auto">
         <a href="/legal/eula" class="link-footer">License agreement</a>
