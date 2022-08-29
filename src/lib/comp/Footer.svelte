2169f6a0810226d3b5f98bfbe42bdbd89bab032b
speechrezz <thespeechlessmusic@gmail.com>
2022-08-29 12:11:24 -0700

    Improved mobile layout

--- src/lib/comp/Footer.svelte
+++ src/lib/comp/Footer.svelte
@@ -1,12 +1,12 @@
 <nav class="bg-base-1 w-full h-16 flex justify-center leading-[normal]">
   <div class="flex justify-center align-middle">
-    <span class="text-xs text-neutral-1 m-auto md:text-base">
-      Copyright © 2022 Xynth Audio. All rights reserved.
-      <!-- <a href="/legal/terms" class="link-footer">Terms & conditions</a> 
-      <span class="text-slate-500">|</span> -->
-      <a href="/legal/eula" class="link-footer">License agreement</a> 
-      <span class="text-slate-500">|</span>
-      <a href="/contact" class="link-footer">Contact</a>
-    </span>
+    <div class="text-xs text-neutral-1 m-auto md:text-base flex flex-col sm:flex-row">
+      <span>Copyright © 2022 Xynth Audio. All rights reserved.</span>
+      <span class="mx-auto">
+        <a href="/legal/eula" class="link-footer">License agreement</a>
+        <span class="text-slate-500">|</span>
+        <a href="/contact" class="link-footer">Contact</a>
+      </span>
+    </div>
   </div>
 </nav>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -17,11 +17,11 @@
 
   <!-- Section 2 -->
   <div class="bg-base-2 w-full flex flex-col align-middle">
-    <div class="w-main flex flex-col align-middle mt-16 mx-auto h-[65vh]">
+    <div class="w-main flex flex-col align-middle mt-16 mx-auto h-fit sm:h-[65vh]">
       <h2 class="text-h2 text-left text-neutral-1 mb-2 font-medium">Contact</h2>
       <p class="text-neutral-1 text-p1"><span class="font-medium">Email:</span> xynthaudio@gmail.com</p>
 
-      <div class="flex justify-around text-neutral-1 text-h2 mt-48">
+      <div class="flex flex-col space-y-8 sm:space-y-0 sm:flex-row justify-around text-neutral-1 text-h2 mt-8 sm:mt-48 mb-8">
         <a href="https://www.instagram.com/xynthaudio/" class="link-footer flex flex-col hover:scale-105">
           <span class="mx-auto">Instagram</span>
           <img src="svg/instagram.svg" alt="Instagram" class="mt-4 w-32 h-32 mx-auto">
